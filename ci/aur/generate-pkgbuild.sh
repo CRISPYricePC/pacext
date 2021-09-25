c24d033643bf6b79704ccc3c1a2febec7a959790
Ben Mitchell <bjosephmitchell@gmail.com>
2021-09-25 15:17:41 +0100

    Generate pkgver to trigger -Syu updates

--- ci/aur/generate-pkgbuild.sh
+++ ci/aur/generate-pkgbuild.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 VERSION=r$(git rev-list --count HEAD).$(git rev-parse --short HEAD)
-sed "s/pkgver=TEMPLATE-VERSION/pkgver=${VERSION}/g" ./ci/aur/PKGBUILD.template > \
+sed "s/pkgver=/pkgver=${VERSION}/g" ./ci/aur/PKGBUILD.template > \
     ./ci/aur/PKGBUILD
